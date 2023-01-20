675e4b3f389b4b750ad5143810fa78979625f65f
kimddogyun <103719413+kimddogyun@users.noreply.github.com>
2023-01-21 01:37:30 +0900

    n
    n

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230121.001838';
+const cacheName = 'chirpy-20230121.013703';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
